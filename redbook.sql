a1d2cc05a70417171e3c25ff9f163e7b3a3a7da6
顾平安 <g332920287@foxmail.com>
2023-10-29 17:02:16 +0800

    update sql

--- redbook.sql
+++ redbook.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `redbook` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `redbook` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 USE `redbook`;
 
