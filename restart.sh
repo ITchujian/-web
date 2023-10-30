e9663f0fcd12fd564d37f5fdfeac5256d7056a23
顾平安 <g332920287@foxmail.com>
2023-10-30 15:04:00 +0800

    add sh

---
+++ restart.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+INI="/projects/xhsweb/backend/xhs_uwsgi.ini"
+UWSGI="/envs/xhs/bin/uwsgi"
+PSID="ps aux | grep "uwsgi"| grep -v "grep" | wc -l"
+
+if [ ! -n "$1" ]
+then
+    content="Usages: sh uwsgiserver.sh [start|stop|restart]"
+    echo -e "\033[31m $content \033[0m"
+    exit 0
+fi
+
+if [ $1 = start ]
+then
+    if [ `eval $PSID` -gt 4 ]
+    then
+        content="uwsgi is running!"
+        echo -e "\033[32m $content \033[0m"
+        exit 0
+    else
+        $UWSGI $INI
+        content="Start uwsgi service [OK]"
+        echo -e "\033[32m $content \033[0m"
+    fi
+
+elif [ $1 = stop ];then
+    if [ `eval $PSID` -gt 4 ];then
+        killall -9 uwsgi
+    fi
+    content="Stop uwsgi service [OK]"
+    echo -e "\033[32m $content \033[0m"
+elif [ $1 = restart ];then
+    if [ `eval $PSID` -gt 4 ];then
+        killall -9 uwsgi
+    fi
+    $UWSGI --ini $INI
+    content="Restart uwsgi service [OK]"
+    echo -e "\033[32m $content \033[0m"
+
+else
+    content="Usages: sh uwsgiserver.sh [start|stop|restart]"
+    echo -e "\033[31m $content \033[0m"
+fi
