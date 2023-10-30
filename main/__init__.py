e98f06db39c1c1df8247f20f5121ccd59f4ae668
顾平安 <g332920287@foxmail.com>
2023-10-30 21:21:58 +0800

    update __init__

--- main/__init__.py
+++ main/__init__.py
@@ -4,20 +4,20 @@
 @Created: 2023/10/25 20:34
 @Description: Created in backend.
 """
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import *
 from .apis.spider import spider_bp
 from .apis.webso import websocket_bp, socketio
 
 
 # 跨域支持
-def after_request(response):
-    # JS前端跨域支持
-    response.headers['Cache-Control'] = 'no-cache'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
-
+def af_request(resp):
+    resp = make_response(resp)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = 'Authorization,content-type,token,id'
+    resp.headers['Access-Control-Request-Headers'] = 'Origin,X-Requested-With,Accept,Authorization'
+    return resp
 
 
 def create_app():
@@ -26,6 +26,6 @@ def create_app():
     app.register_blueprint(spider_bp, url_prefix='/api/spider')
     app.register_blueprint(websocket_bp, url_prefix='/monitor')
     socketio.init_app(app, cors_allowed_origins='*')
-    app.after_request(after_request)
+    app.after_request(af_request)
     CORS(app)
     return app, socketio
