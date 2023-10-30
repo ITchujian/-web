a5cb4a8719255c8b0cf17ff8026262b0c1067b3f
顾平安 <g332920287@foxmail.com>
2023-10-30 21:25:22 +0800

    delete lines

--- main/__init__.py
+++ main/__init__.py
@@ -10,21 +10,11 @@ from .apis.spider import spider_bp
 from .apis.webso import websocket_bp, socketio
 
 
-# 跨域支持
-def af_request(resp):
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.headers['Access-Control-Allow-Methods'] = '*'
-    resp.headers['Access-Control-Allow-Headers'] = 'Authorization,content-type,token,id'
-    resp.headers['Access-Control-Request-Headers'] = 'Origin,X-Requested-With,Accept,Authorization'
-    return resp
-
-
 def create_app():
     app = Flask(__name__)
     app.secret_key = '20010908gupingan'
     app.register_blueprint(spider_bp, url_prefix='/api/spider')
     app.register_blueprint(websocket_bp, url_prefix='/monitor')
     socketio.init_app(app, cors_allowed_origins='*')
-    app.after_request(af_request)
     CORS(app)
     return app, socketio
